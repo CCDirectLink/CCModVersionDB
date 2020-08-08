1039bf222ff591046a24c61e4a66766fbcbd7f31
ac2pic <ac2pic@gmail.com>
2020-08-08 17:11:29 -0500

    Duplicate versions no longer occur

--- scripts/generate-mod-entry.js
+++ scripts/generate-mod-entry.js
@@ -81,109 +81,146 @@ function generateReleaseChangeLog(changelogText, version)  {
     }
     return releases[version];
 }
-for (const modPath of modPaths) {
-    // if mod does not have a .dbid file then generate one
-    const dbIdPath = path.join(modPath, '.dbid');
-    let uuid;
-    try {
-        uuid = fs.readFileSync(dbIdPath);
-    } catch (e) {
-        if (e.code === 'ENOENT') {
-            uuid = uuidv4().replace(/-/g, '');
-            fs.writeFileSync(dbIdPath, uuid);
-        } else {
+
+// Modified version of script on: https://blog.abelotech.com/posts/calculate-checksum-hash-nodejs-javascript/
+function calculateHash(fileName) {
+    const hash = crypto.createHash('sha256');
+    const ccmodFile = fs.createReadStream(fileName);
+
+    return new Promise((resolve, reject) => {
+        ccmodFile.on('data', (data) => hash.update(data, 'utf8'))
+        ccmodFile.on('end', () => resolve(hash.digest('hex')))
+    });
+}
+
+function findVersionInReleases(releases, version) {
+    for (let i = 0; i < releases.length; i++) {
+        if (releases[i].version === version) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+async function run() {
+    for (const modPath of modPaths) {
+        // if mod does not have a .dbid file then generate one
+        const dbIdPath = path.join(modPath, '.dbid');
+        let uuid;
+        try {
+            uuid = fs.readFileSync(dbIdPath);
+        } catch (e) {
+            if (e.code === 'ENOENT') {
+                uuid = uuidv4().replace(/-/g, '');
+                fs.writeFileSync(dbIdPath, uuid);
+            } else {
+                throw e;
+            }
+        }
+    
+        const pathToVerboseConfig = path.join(process.cwd(),`/mods/${uuid}.json`);
+        let config = {};
+        try {
+            config = JSON.parse(fs.readFileSync(pathToVerboseConfig, 'utf8'));
+        } catch (e) {
+            if (e.code !== 'ENOENT') {
+                console.error(e);
+            }
+        }
+    
+        // grab id, name, description from mod ccmod.json file
+        // and add it to generated file entry
+        const manifestPath = path.join(modPath, 'ccmod.json');
+    
+        let manifest;
+        try {
+            manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
+        } catch (e) {
             throw e;
         }
-    }
+    
+        const release = {
+            id: manifest.id,
+            version: manifest.version,
+            dependencies: manifest.dependencies || {}
+        };
 
-    const pathToVerboseConfig = path.join(process.cwd(),`/mods/${uuid}.json`);
-    let config = {};
-    try {
-        config = JSON.parse(fs.readFileSync(pathToVerboseConfig, 'utf8'));
-    } catch (e) {
-        console.error(e);
-    }
-
-    // grab id, name, description from mod ccmod.json file
-    // and add it to generated file entry
-    const manifestPath = path.join(modPath, 'ccmod.json');
-
-    let manifest;
-    try {
-        manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
-    } catch (e) {
-        throw e;
-    }
-
-    const release = {
-        id: manifest.id,
-        version: manifest.version,
-        dependencies: manifest.dependencies || {}
-    };
-
-    if (manifest.id) {
-        config.id = manifest.id;
-    }
-
-    if (manifest.name) {
-        config.name = strToLocaleObject(manifest.name);
-    }
-
-    if (manifest.description) {
-        config.description = strToLocaleObject(manifest.description);
-    }
-
-    // grab tags, pages, contributors, latest-version from dbmetadata.json
-
-    const metadataPath = path.join(modPath, 'dbmetadata.json');
-
-    let metadata;
-    try {
-        metadata = JSON.parse(fs.readFileSync(metadataPath, 'utf8'));
-        if (Array.isArray(metadata.tags)) {
-            config.tags = metadata.tags;
+        const version = manifest.version.replace('v', '');
+        
+        if (manifest.id) {
+            config.id = manifest.id;
         }
     
-        if (Array.isArray(metadata.pages)) {
-            config.pages = metadata.pages;
+        if (manifest.name) {
+            config.name = strToLocaleObject(manifest.name);
         }
     
-        if (Array.isArray(metadata.contributors)) {
-            config.contributors = metadata.contributors;
+        if (manifest.description) {
+            config.description = strToLocaleObject(manifest.description);
         }
     
-        if (metadata["latest-version"] && typeof metadata["latest-version"]  === "object") {
-            config["latest-version"] = metadata["latest-version"];
-        }
-        // generate archive_links
-        release.archive_links = generateArchiveLinks(metadata.archive_links, manifest);
-
-        // use "release-file" to generate ccmod hash
-        // can only do it for latest verion, assume
-        if (metadata["release-file"]) {
-            
-        }
-    } catch (e) {}
+        // grab tags, pages, contributors, latest-version from dbmetadata.json
+        const metadataPath = path.join(modPath, 'dbmetadata.json');
     
-
-    if (!Array.isArray(config.releases)) {
-        config.releases = [];
+        let metadata;
+        try {
+            metadata = JSON.parse(fs.readFileSync(metadataPath, 'utf8'));    
+        } catch (e) {}
+        
+        if (metadata) {
+            if (Array.isArray(metadata.tags)) {
+                config.tags = metadata.tags;
+            }
+        
+            if (Array.isArray(metadata.pages)) {
+                config.pages = generateArchiveLinks(metadata.pages, manifest);
+            }
+        
+            if (Array.isArray(metadata.contributors)) {
+                config.contributors = metadata.contributors;
+            }
+        
+            if (metadata["latest-version"] && typeof metadata["latest-version"]  === "object") {
+                config["latest-version"] = metadata["latest-version"];
+            }
+    
+            // generate archive_links
+            release.archive_links = generateArchiveLinks(metadata.archive_links, manifest);
+    
+            // use "release-file" to generate ccmod hash
+            // can only do it for latest verion
+            if (metadata["release-file"]) {
+                if (!release.hash) {
+                    release.hash = {};
+                }
+                release.hash['sha256'] = await calculateHash(metadata["release-file"]);
+            }
+        }
+    
+        if (!Array.isArray(config.releases)) {
+            config.releases = [];
+        }
+    
+        const versionReleaseIndex = findVersionInReleases(config.releases, version);
+        if (versionReleaseIndex === -1) {
+            config.releases.push(release);
+        } else {
+            config.releases.splice(versionReleaseIndex, 1, release);
+        }
+        
+        const changelogPath = path.join(modPath, 'CHANGELOG');
+    
+        try {
+            // generate release changelog from CHANGELOG
+            const changelogRaw = fs.readFileSync(changelogPath, 'utf8');
+            const changelogData = generateReleaseChangeLog(changelogRaw, version);
+            Object.assign(release, changelogData);
+        } catch (e) {}
+        
+        fs.writeFileSync(pathToVerboseConfig, JSON.stringify(config));
     }
-
-    config.releases.push(release);
-
-    const changelogPath = path.join(modPath, 'CHANGELOG');
-
-    try {
-        // generate release changelog from CHANGELOG
-        const changelogRaw = fs.readFileSync(changelogPath, 'utf8');
-        const changelogData = generateReleaseChangeLog(changelogRaw, manifest.version.replace('v', ''));
-        Object.assign(release, changelogData);
-    } catch (e) {}
-
-    fs.writeFileSync(pathToVerboseConfig, JSON.stringify(config));
+    
 }
 
 
-
-
+run()
